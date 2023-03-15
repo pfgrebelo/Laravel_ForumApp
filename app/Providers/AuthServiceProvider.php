1d397557cdefe0a802f7b3fc586d61cde28ab5cc
Pedro Rebelo <pfiliperebelo@gmail.com>
2023-03-15 15:12:00 +0000

    added post policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -13,6 +13,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
+        Post::class => PostPolicy::class
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
     ];
 
--- resources/views/single-post.blade.php
+++ resources/views/single-post.blade.php
@@ -1,22 +1,26 @@
 <x-layout>
-    <div class="container py-md-5 container--narrow">
-        <div class="d-flex justify-content-between">
-          <h2>{{$post->title}}</h2>
-          <span class="pt-2">
-            <a href="#" class="text-primary mr-2" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fas fa-edit"></i></a>
-            <form class="delete-post-form d-inline" action="#" method="POST">
-              <button class="delete-post-button text-danger" data-toggle="tooltip" data-placement="top" title="Delete"><i class="fas fa-trash"></i></button>
-            </form>
-          </span>
-        </div>
-  
-        <p class="text-muted small mb-4">
-          <a href="#"><img class="avatar-tiny" src="https://gravatar.com/avatar/f64fc44c03a8a7eb1d52502950879659?s=128" /></a>
-          Posted by <a href="#">{{$post->user->username}}</a> on {{$post->created_at->format('d/m/Y')}}
-        </p>
-  
-        <div class="body-content">
-          {!! $post->body !!}
-        </div>
-      </div>
+  <div class="container py-md-5 container--narrow">
+    <div class="d-flex justify-content-between">
+      <h2>{{$post->title}}</h2>
+      @can('update', $post)
+      <span class="pt-2">
+        <a href="#" class="text-primary mr-2" data-toggle="tooltip" data-placement="top" title="Edit"><i
+            class="fas fa-edit"></i></a>
+        <form class="delete-post-form d-inline" action="#" method="POST">
+          <button class="delete-post-button text-danger" data-toggle="tooltip" data-placement="top" title="Delete"><i
+              class="fas fa-trash"></i></button>
+        </form>
+      </span>
+      @endcan
+    </div>
+    <p class="text-muted small mb-4">
+      <a href="#"><img class="avatar-tiny"
+          src="https://gravatar.com/avatar/f64fc44c03a8a7eb1d52502950879659?s=128" /></a>
+      Posted by <a href="#">{{$post->user->username}}</a> on {{$post->created_at->format('d/m/Y')}}
+    </p>
+
+    <div class="body-content">
+      {!! $post->body !!}
+    </div>
+  </div>
 </x-layout>
