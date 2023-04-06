0c3ae2db603c584460d3ed4c3d3a6a9e99590e36
Pedro Rebelo <pfiliperebelo@gmail.com>
2023-04-06 15:57:28 +0100

    add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,62 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'id' => 1,
+            'username' => 'pedro',
+            'email' => 'pedro@mail.com',
+            'password' => Hash::make('password'),
+            'isAdmin' => 1
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('users')->insert([
+            'id' => 2,
+            'username' => 'barksalot',
+            'email' => 'barksalot@mail.com',
+            'password' => Hash::make('password')
+        ]);
+
+        DB::table('users')->insert([
+            'id' => 3,
+            'username' => 'meowsalot',
+            'email' => 'meowsalot@mail.com',
+            'password' => Hash::make('password')
+        ]);
+
+        DB::table('posts')->insert([
+            'user_id' => 1,
+            'title' => 'My First Post',
+            'body' => 'Lorem ipsum this is my post.',
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('posts')->insert([
+            'user_id' => 1,
+            'title' => 'My Second Post: HTML',
+            'body' => 'HTML stands for Hyper Text Markup Language',
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('posts')->insert([
+            'user_id' => 2,
+            'title' => 'Being a Dog Is Fun',
+            'body' => 'I like to run and bark.',
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('follows')->insert([
+            'user_id' => 2,
+            'followeduser' => 1
+        ]);
+
+        DB::table('follows')->insert([
+            'user_id' => 3,
+            'followeduser' => 1
+        ]);
+
+        DB::table('follows')->insert([
+            'user_id' => 3,
+            'followeduser' => 2
+        ]);
     }
 }
