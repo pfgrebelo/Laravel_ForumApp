89f601257acdae8c3788d00049eaa21724083bc6
Pedro Rebelo <pfiliperebelo@gmail.com>
2023-05-23 23:20:49 +0100

    scheduling tasks app\console\kernel php artisan schedule:work

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Mail\RecapEmail;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -12,6 +14,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
+        $schedule->call(function(){
+            Mail::to('admin@google.com')->send(new RecapEmail());
+        })->everyMinute();
         // $schedule->command('inspire')->hourly();
     }
 
