6018333aac0ecb651f5d7df5416314c53b76ace4
Pedro Rebelo <pfiliperebelo@gmail.com>
2023-05-23 22:45:59 +0100

    added sending email used mailtrap php artisan make:mail NewPostEmail

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Mail\NewPostEmail;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class PostController extends Controller
 {
@@ -61,6 +63,8 @@ class PostController extends Controller
         $incomingFields['user_id'] = auth()->id();
 
         $newPost = Post::create($incomingFields);
+
+        Mail::to(auth()->user()->email)->send(new NewPostEmail(['name' => auth()->user()->username, 'title' => $newPost->title]));
 
         return redirect("/post/{$newPost->id}")->with('success', 'New post successfully created.');
     }
